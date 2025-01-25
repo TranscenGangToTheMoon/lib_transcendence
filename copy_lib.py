ef8293d3776d593cf4057f24ecc613a636c99c06
fguirama <florian.guiramand@protonmail.com>
2025-01-25 12:43:14 +0100

    check if lib_transcendence are the same in all services before copy new lib_transcendence

--- copy_lib.py
+++ copy_lib.py
@@ -1,3 +1,4 @@
+import filecmp
 import os
 import shutil
 
@@ -7,6 +8,23 @@ services = ['auth', 'chat', 'game', 'matchmaking', 'users']
 
 if not os.path.exists(service_path):
     exit(1)
+
+
+def diff_dir(dir1, dir2):
+    diff = filecmp.dircmp(dir1, dir2)
+
+    if diff.diff_files or diff.left_only or diff.right_only:
+        exit(1)
+
+    for subdir in diff.subdirs.values():
+        diff_dir(os.path.join(dir1, subdir.left), os.path.join(dir2, subdir.right))
+
+
+dir_ref = f'{service_path}/{services[0]}/django/lib_transcendence'
+for service in services[1:]:
+    print('checking diff :', services[0], '==', service)
+    diff_dir(dir_ref, f'{service_path}/{service}/django/lib_transcendence')
+
 
 for service in services:
     dst = f'{service_path}/{service}/django/lib_transcendence'
